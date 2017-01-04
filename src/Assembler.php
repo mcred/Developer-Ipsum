640ec5b025032f823ee8d0e55d57396d5824e7e0
Derek Smart <derek@grindaga.com>
2017-01-04 16:01:40 -0500

    base assembler and test
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/Assembler.php
+++ src/Assembler.php
@@ -7,42 +7,84 @@
  */
 class Assembler
 {
-    public function __construct()
+    private $proper_nouns;
+    private $verbs;
+    private $conjuctions;
+    private $prepositions;
+    private $directObjects;
+    private $sentencePatterns;
+
+    public function __construct(Vocabulary $vocab)
     {
+        $this->proper_nouns = $vocab->setProperNouns();
+        $this->verbs = $vocab->setVerbs();
+        $this->conjuctions = $vocab->setConjunctions();
+        $this->prepositions = $vocab->setPrepositions();
+        $this->directObjects = $vocab->setDirectObjects();
+        $this->sentencePatterns = $this->setSentencePatterns();
     }
 
-    public function getRandomWord()
+    private function setSentencePatterns()
     {
-        $words = [
-            'World class',
-            'API’s',
-            'and',
-            'tabs vs. spaces',
-            'integrate',
-            'with',
-            'your',
-            'developer',
-            'in',
-            'an',
-            'agile',
-            'standing desk',
-            'GitHub',
-            'coffee breaks',
-            'no',
-            'ego',
-            'methodology',
-            'deploys',
-            'to',
-            'waterfall',
-            'ops',
-            'Whiteboard',
-            'sprint',
-            'the',
-            'nutter butter',
-            'stash',
-            'server',
-            'configuration'
+        return [
+            'PN,C,PN,V,P,DO',
+            'PN,V,P,DO,C,P,DO',
+            'PN,V,P,DO'
         ];
-        return $words[rand(0, count($words) - 1)];
+    }
+
+    private function getWordFromPart(string $part) : string
+    {
+        switch ($part) {
+            case 'PN':
+                $key = array_rand($this->proper_nouns);
+                $word = $this->proper_nouns[$key];
+                unset($this->proper_nouns[$key]);
+                return $word;
+                break;
+            case 'V':
+                $key = array_rand($this->verbs);
+                $word = $this->verbs[$key];
+                unset($this->verbs[$key]);
+                return $word;
+                break;
+            case 'C':
+                $key = array_rand($this->conjuctions);
+                $word = $this->conjuctions[$key];
+                unset($this->conjuctions[$key]);
+                return $word;
+                break;
+            case 'P':
+                $key = array_rand($this->prepositions);
+                $word = $this->prepositions[$key];
+                unset($this->prepositions[$key]);
+                return $word;
+                break;
+            case 'DO':
+                $key = array_rand($this->directObjects);
+                $word = $this->directObjects[$key];
+                unset($this->directObjects[$key]);
+                return $word;
+                break;
+            default:
+                throw new InvalidArgumentException('Not a valid sentence part.');
+                break;
+        }
+    }
+
+    private function getRandomSentencePattern()
+    {
+        return $this->sentencePatterns[array_rand($this->sentencePatterns)];
+    }
+
+    public function createSentence()
+    {
+        $pattern = explode(',', $this->getRandomSentencePattern());
+
+        $return = '';
+        foreach ($pattern as $part) {
+            $return .= $this->getWordFromPart($part) . ' ';
+        }
+        return rtrim($return) . '.';
     }
 }
---
+++ tests/unit/AssemblerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+* @covers Assembler
+* @covers Vocabulary
+*/
+class AssemblerTest extends \PHPUnit\Framework\TestCase
+{
+    private $Assembler;
+
+	public function setup()
+	{
+        $this->Assembler = new Assembler(new Vocabulary());
+	}
+
+    public function testCanInstantiateAssembler()
+    {
+        $this->assertInstanceOf(Assembler::class, $this->Assembler);
+    }
+
+    public function testCreateSentence()
+    {
+        var_dump($this->Assembler->createSentence());
+    }
+}
