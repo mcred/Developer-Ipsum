eabcac7090ef5c58a2f3ab91e4f295eb1f73df25
Derek Smart <derek@grindaga.com>
2017-01-04 17:38:05 -0500

    genrate file to use factory
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- generate.php
+++ generate.php
@@ -2,6 +2,6 @@
 require(__DIR__."/vendor/autoload.php");
 require(__DIR__."/src/autoload.php");
 
-$paragraphs = new Paragraphs(new Assembler(new Vocabulary()), 4, 8);
+$paragraphs = IpsumFactory::create('paragraphs', 4, 8);
 echo $paragraphs->generate();
 ?>
