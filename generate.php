98fe50bc6abafb397f2428f8991fbe7dc6ac1067
Derek Smart <derek@grindaga.com>
2017-01-06 11:31:05 -0500

    update generator for injected vocab
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- generate.php
+++ generate.php
@@ -1,7 +1,11 @@
 <?php
 require(__DIR__."/vendor/autoload.php");
 require(__DIR__."/src/autoload.php");
+require(__DIR__."/config/vocab.php");
 
-$paragraphs = IpsumFactory::create('paragraphs', 4, 8);
+$vocab = new VOCAB();
+$factory = new IpsumFactory($vocab);
+$paragraphs = $factory->create('paragraphs', 4, 8);
 echo $paragraphs->generate();
+
 ?>
