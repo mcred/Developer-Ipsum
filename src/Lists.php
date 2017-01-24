15af8250a847bdceddec3da409255fd1e1256bf5
Derek Smart <derek@grindaga.com>
2017-01-24 17:59:52 -0500

    remove constructor to inherit from parent
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- src/Lists.php
+++ src/Lists.php
@@ -8,18 +8,6 @@
 class Lists extends Ipsum
 {
     /**
-     * Constructor of Lists
-     *
-     * @param Assembler $assembler
-     * @param int       $count
-     * @param int       $length
-     */
-    public function __construct(Assembler $assembler, int $count, int $length)
-    {
-        parent::__construct($assembler, $count, $length);
-    }
-    
-    /**
      * create properly formatted Lists
      *
      * @return string
